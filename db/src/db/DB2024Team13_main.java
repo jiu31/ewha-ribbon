e0363f090b7f1a10266b2a33ee2461c93d0072df
jung <jung@JiwoosLaptop>
2024-05-28 18:36:36 +0900

    login

---
+++ db/src/db/DB2024Team13_main.java
@@ -0,0 +1,10 @@
+package db;
+
+import db.view.DB2024Team13_loginWindow;
+
+public class DB2024Team13_main {
+    public static void main(String[] args) {
+        // 로그인 창 표시
+        DB2024Team13_loginWindow.displayLoginWindow();
+    }
+}
---
+++ db/src/db/model/DB2024Team13_login.java
@@ -0,0 +1,32 @@
+package db.model;
+
+import db.session.DB2024Team13_userSession;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DB2024Team13_login {
+
+    public static boolean validateLogin(String studentId, String password) {
+        String query = "SELECT * FROM DB2024_customer WHERE student_id = ? AND password = ?";
+        Connection connection = DB2024Team13_connection.getConnection();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, studentId);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String nickname = resultSet.getString("nickname");
+                    DB2024Team13_userSession.getInstance().setUser(studentId, nickname);
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
